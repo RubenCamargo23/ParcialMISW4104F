b8e41504478ae493071924212e5348bd8a720d48
Eduardo Camargo <r.camargoo@uniandes.edu.co>
2025-10-31 14:44:03 -0500

    ✨Feat (Vehiculo): Calculo de marcas

---
+++ src/app/vehiculo/brandtotal.ts
@@ -0,0 +1,4 @@
+export interface BrandTotal {
+  marca: string;
+  total: number;
+}
--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Vehiculo} from "../vehiculo";
 import {VehiculoService} from "../vehiculo.service";
+import {BrandTotal} from "../brandtotal";
 
 @Component({
   selector: 'app-vehiculo-list',
@@ -9,13 +10,35 @@ import {VehiculoService} from "../vehiculo.service";
 })
 export class VehiculoListComponent {
   vehiculos: Array<Vehiculo> = [];
+  brandTotals: Array<BrandTotal> = [];
 
   constructor(private vehiculosService: VehiculoService) { }
 
   getVehiculosList() {
     this.vehiculosService.getVehiculos().subscribe(vehiculos => {
       this.vehiculos = vehiculos;
+      this.calculateBrandTotals();
     });
+  }
+
+  calculateBrandTotals() {
+    const totalsMap: { [key: string]: number } = {};
+
+    this.vehiculos.forEach(vehiculo => {
+      const marca = vehiculo.marca;
+      if (totalsMap[marca]) {
+        totalsMap[marca]++;
+      } else {
+        totalsMap[marca] = 1;
+      }
+    });
+
+    this.brandTotals = Object.keys(totalsMap)
+      .sort()
+      .map(marca => ({
+        marca: marca,
+        total: totalsMap[marca]
+      }));
   }
 
   ngOnInit() {
